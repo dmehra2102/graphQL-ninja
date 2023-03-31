4fb97924f946c7941489c7487c4a496f15922bf7
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2023-03-31 09:53:09 +0530

    apollo/client setup is done

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AddBook from './components/AddBook'
+import BookList from './components/BookList'
+
+
+function App() {
+
+  return (
+    <div id="main">
+      <BookList />
+      <AddBook />
+    </div>
+  )
+}
+
+export default App
